b25b9fb088f49f8dd8fd10b1f76092f76f2d62f5
yuhk1 <yuhk1.nakamura7@gmail.com>
2020-04-21 06:54:48 +0000

    user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root to: "tasks#index"
   
+  get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :new, :create]
+  
   resources :tasks
 end
