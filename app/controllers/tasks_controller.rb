43ce23e26e4ff67fd20434d608a3f060ce3bae2d
yuhk1 <yuhk1.nakamura7@gmail.com>
2020-04-21 14:26:55 +0000

    user done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,13 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:edit, :update, :destroy]
+  
   def index
     @tasks =Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
+    @task = Task.find_by(params[:id])
   end
 
   def new
@@ -12,13 +15,13 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
-
+    @task = current_user.tasks.build(task_params)
     if @task.save
-      flash[:success] = "Task が正常に投稿されました"
-      redirect_to @task
+      flash[:success] = '投稿しました。'
+      redirect_to root_url
     else
-      flash.now[:danger] = "Task が投稿されませんでした"
+      #@tasks = current_user.tasks.order(id: :desc).page(params[:page])
+      flash.now[:danger] = '投稿に失敗しました。'
       render :new
     end
   end
@@ -46,8 +49,21 @@ class TasksController < ApplicationController
     flash[:success] = 'Task は正常に削除されました'
     redirect_to tasks_url
   end
-  
+
   private
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+  end
+  
+  def require_user_logged_in
+    unless logged_in?
+      redirect_to login_url
+    end
+  end
 
   def task_params
     params.require(:task).permit(:content, :status)
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_21_061631) do
+ActiveRecord::Schema.define(version: 2020_04_21_091919) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.string "status"
     t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
