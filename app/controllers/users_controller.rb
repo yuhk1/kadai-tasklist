fbe23e31f96dc04d5f356a3fc12afaf2f07997dc
yuhk1 <yuhk1.nakamura7@gmail.com>
2020-04-24 02:49:24 +0000

    tasksurl add

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
 
     if @user.save
       flash[:success] = 'ユーザを登録しました。'
-      redirect_to sessions_url
+      redirect_to  tasks_url
     else
       flash.now[:danger] = 'ユーザの登録に失敗しました。'
       render :new
