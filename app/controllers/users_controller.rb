97e44730fc86b03799d8a30264d1c69efd28455a
yuhk1 <yuhk1.nakamura7@gmail.com>
2020-04-24 02:24:36 +0000

    sessionsurl add

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
 
     if @user.save
       flash[:success] = 'ユーザを登録しました。'
-      redirect_to @user
+      redirect_to sessions_url
     else
       flash.now[:danger] = 'ユーザの登録に失敗しました。'
       render :new
