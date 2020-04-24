4db7fa980556ef99f1946dfb871de8f2416aef18
yuhk1 <yuhk1.nakamura7@gmail.com>
2020-04-24 08:47:29 +0000

    taskcontroller fix

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
-  before_action :correct_user, only: [:edit, :update, :destroy]
+  before_action :correct_user, only: [:index, :show, :edit, :update, :destroy]
   
   def index
     @tasks = current_user.tasks
